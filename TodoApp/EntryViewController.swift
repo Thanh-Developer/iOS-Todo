a776596dd2ef945f0368b31f8eaaa4ea63c7bd25
JohnPham <ThanhP@joblogic.com>
2023-10-30 23:20:05 +0700

    Format code

--- TodoApp/EntryViewController.swift
+++ TodoApp/EntryViewController.swift
@@ -7,23 +7,53 @@
 
 import UIKit
 
-class EntryViewController: UIViewController {
-
+class EntryViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var field: UITextField!
+    var update: (() -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        field.delegate = self
+        
+        // Listener save action
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "save", style: .done, target: self, action: #selector(saveTask))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        
+        saveTask()
+        
+        return true
     }
-    */
-
+    
+    @objc func saveTask(){
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        let newCount = count + 1
+        
+        UserDefaults().set(newCount, forKey: "count")
+        UserDefaults().set(text, forKey: "task_\(newCount)")
+        
+        update?()
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- TodoApp/TaskViewController.swift
+++ TodoApp/TaskViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class TaskViewController: UIViewController {
-
+    
+    @IBOutlet var label: UILabel!
+    var task:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        label.text = task
+        
+        // Listener delete action
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "delete", style: .done, target: self, action: #selector(deleteTask))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func deleteTask(){
+        
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
